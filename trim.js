71917936027c7b4971d1a357f33f5bef6ac62936
Dev-ayman74 <104323355+Dev-ayman74@users.noreply.github.com>
2022-04-25 02:44:00 +0200

    Update trim.js

--- trim.js
+++ trim.js
@@ -1,1 +1,31 @@
+const trimArray = " Web development ";
+
+console.log(` 1- Original String: ${trimArray}`);
+// Method trim()
+
+console.log(` 2- Trimmed String : ${trimArray.trim()}`);
+
+// another methods such as padStart() and padEnd()
+
+const start = "starting point";
+const middle = start.padStart(40);
+console.log(start);
+console.log(middle); // Will be pushed 30 tabs from start
+console.log(middle.length);
+/**
+ * output: 
+ *  1- Original String:  Web development 
+ 2- Trimmed String : Web development
+starting point
+                starting point
+ */
+
+const firstName = "PERSON";
+const lastName = "john";
+
+console.log(` 
+Firstname: LOWERCASE ${firstName.toLocaleLowerCase()}
+\n
+Lastname: ${lastName.toLocaleUpperCase()} 
+`);
 
