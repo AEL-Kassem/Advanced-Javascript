ecf72d3739a374fb0bfea25cb024e4cd0c1fa9c7
Dev-ayman74 <104323355+Dev-ayman74@users.noreply.github.com>
2022-04-25 02:50:12 +0200

    Create charAt.js

---
+++ charAt.js
@@ -0,0 +1,12 @@
+// charAt()
+
+const developer = 'I am a developer';
+const index= 8
+console.log(`The index located at :
+ ${developer.charAt(index)}`)
+ /**
+  * 'I am a developer '
+  * index I is Zero 
+  */
+
+ 
