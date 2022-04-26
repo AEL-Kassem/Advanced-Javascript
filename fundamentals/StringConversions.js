8a2db28af859bdb8800cd0ba7fbc8b2beced67fb
Dev-ayman74 <104323355+Dev-ayman74@users.noreply.github.com>
2022-04-27 01:43:52 +0200

    Create StringConversions.js

---
+++ fundamentals/StringConversions.js
@@ -0,0 +1,12 @@
+let summary = true;
+alert(typeof summary) // Boolean
+
+summary = String(summary);
+alert(typeof summary) ; // String
+
+
+let answer = '244';
+alert(typeof answer) ; // String
+
+let question = Number(answer); // 244
+alert(typeof question) ; // Number
