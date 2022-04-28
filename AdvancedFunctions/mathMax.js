fad33b025f3c13b535b7bf939eeb0dcaaf67ded4
Dev-ayman74 <104323355+Dev-ayman74@users.noreply.github.com>
2022-04-29 00:45:03 +0200

    Update mathMax.js

--- AdvancedFunctions/mathMax.js
+++ AdvancedFunctions/mathMax.js
@@ -18,3 +18,26 @@ let ex2=[2,4,16,17,8];
 let ex3= [12,13,14,15];
 
 console.log(Math.max(...ex1, ...ex2, ...ex3)); // Output: 17
+
+// Spread syntax can be used to merge array. forexample:
+
+let simple1 = [1,2,3,4];
+let simple2 = [2,3,5,7];
+
+let mergedArray = [0, ...simple1, 3, ...simple2  ]
+console.log(mergedArray); 
+/**
+ * OUTPUT:
+ * (10) [0, 1, 2, 3, 4, 3, 2, 3, 5, 7]
+0: 0
+1: 1
+2: 2
+3: 3
+4: 4
+5: 3
+6: 2
+7: 3
+8: 5
+9: 7
+length: 10
+ */
